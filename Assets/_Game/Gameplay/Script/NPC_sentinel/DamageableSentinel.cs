3c1b5bdca7aab650b429db2f1829585562dfc723
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2022-02-09 18:37:26 -0300

    onCollisionEffect audio 3D added.

---
+++ Assets/_Game/Gameplay/Script/NPC_sentinel/DamageableSentinel.cs
@@ -0,0 +1,68 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageableSentinel : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float hp;
+    [SerializeField] private Image lifeBarFill;
+    PhotonView PV;
+    Animator animator;
+    [SerializeField] private AudioSource audioSource;
+    public AudioClip deathClip;
+    [SerializeField] private ResetSentinel resetSentinel;
+
+    public float HP { get => hp; set => hp = value; }
+    private float maxHP;
+
+    private float hpPercent => hp / maxHP;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+        animator = GetComponent<Animator>();
+        //audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    private void Start()
+    {
+        maxHP = HP;
+    }
+
+
+
+    [PunRPC]
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        lifeBarFill.fillAmount = hpPercent;
+        
+        CheckDeath();
+    }
+
+    private void CheckDeath()
+    {
+        if(hp <= 0)
+        {
+           //PV.RPC("DeathEvent", RpcTarget.All );
+            DeathEvent();
+        }
+    }
+
+    //[PunRPC]
+    public void DeathEvent()
+    {
+        audioSource.clip = deathClip;
+        audioSource.Play();
+        hp = maxHP;
+        resetSentinel.ResetingSentinel();
+        gameObject.SetActive(false);
+
+    }
+
+
+
+}
---
+++ Assets/_Game/Gameplay/Script/NPC_sentinel/ResetSentinel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetSentinel : MonoBehaviour
+{
+
+    [SerializeField] private GameObject setinel;
+
+    SentinelController sentinelController;
+    DamageableSentinel damageableSentinel;
+    private void Awake()
+    {
+        sentinelController = setinel.GetComponent<SentinelController>();
+        damageableSentinel = setinel.GetComponent<DamageableSentinel>();
+
+    }
+
+
+    public IEnumerator ResetingSentinel()
+    {
+        yield return new WaitForSeconds(GameConfigs.instance.resetSentineltime);
+        setinel.SetActive(true);
+    }
+
+
+
+
+}
