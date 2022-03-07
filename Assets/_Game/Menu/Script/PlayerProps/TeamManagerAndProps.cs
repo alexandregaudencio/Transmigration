02091c502e4e49ae54bf69acdd2dc25b4717a777
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2022-03-07 11:54:52 -0300

    FrameRateTarget, FPS and Ping.

--- Assets/_Game/Menu/Script/PlayerProps/TeamManagerAndProps.cs
+++ Assets/_Game/Menu/Script/PlayerProps/TeamManagerAndProps.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
+using System.Collections;
 using UnityEngine;
 
 public class TeamManagerAndProps : PhotonTeamsManager
@@ -8,14 +9,31 @@ public class TeamManagerAndProps : PhotonTeamsManager
     private ExitGames.Client.Photon.Hashtable hasIndexPlayer = new ExitGames.Client.Photon.Hashtable();
     Player[] playersTeam;
 
-    private int FPS;
+    private int ping;
+    private float fps;
+    private float dt = 0.00f;
+    [SerializeField] private int targetFrameRate;
 
     private void Start()
     {
         DontDestroyOnLoad(this.gameObject);
-        InvokeRepeating("SetFPS", 1, 1);
+        StartCoroutine(SetFpsAndPing());
+        Application.targetFrameRate = targetFrameRate;
+
 
     }
+    private void OnDestroy()
+    {
+        StopCoroutine(SetFpsAndPing());
+    }
+
+
+    //private void Update()
+    //{
+    //    ping = PhotonNetwork.GetPing();
+    //    fps = (float)System.Math.Round(1.0f / dt, 1);
+    //    dt += (Time.deltaTime - dt) * 0.1f;
+    //}
 
     //define time e index do player
     public void SetTeam(Player newPlayer)
@@ -46,21 +64,30 @@ public class TeamManagerAndProps : PhotonTeamsManager
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 100, 100), "Ping: " + PhotonNetwork.GetPing());
+        GUI.Label(new Rect(10, 10, 100, 100), "Ping: " + ping);
+        GUI.Label(new Rect(10, 25, 100, 100), "FPS: " + fps);
 
-        GUI.Label(new Rect(10, 30, 300, 100), "Players: "+PhotonNetwork.CurrentRoom?.PlayerCount);
-        GUI.Label(new Rect(10, 40, 300, 100), "TeamA: "+GetTeamMembersCount(1).ToString());
-        GUI.Label(new Rect(10, 50, 300, 100), "TeamB: " + GetTeamMembersCount(2).ToString());
-        GUI.Label(new Rect(10, 70, 300, 100), "Region: " + PhotonNetwork.CloudRegion);
+        GUI.Label(new Rect(10, 50, 300, 100), "Players: "+PhotonNetwork.CurrentRoom?.PlayerCount);
+        GUI.Label(new Rect(10, 60, 300, 100), "TeamA: "+GetTeamMembersCount(1).ToString());
+        GUI.Label(new Rect(10, 70, 300, 100), "TeamB: " + GetTeamMembersCount(2).ToString());
 
         //GUI.Label(new Rect(10, 20, 100, 100), "FPS: " + FPS);
 
     }
 
-    private void SetFPS()
-    { 
-      FPS =  (int)(1f / Time.unscaledDeltaTime);
+
+
+    private IEnumerator SetFpsAndPing()
+    {
+        while (true)
+        {
+            ping = PhotonNetwork.GetPing();
+            fps =  (int)(1f / Time.unscaledDeltaTime);
+            yield return new WaitForSeconds(0.2f);
+        }
+
     }
+
 
 
 
