79f28c3b88d5fa9f7c1b13f86c899d37aae268e2
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2022-04-29 19:28:23 -0300

    Characters: PlayerAudionManager

--- Assets/_Game/Gameplay/Script/Player/Controller/PlayerAudioManager.cs
+++ Assets/_Game/Gameplay/Script/Player/Controller/PlayerAudioManager.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum PlayerAudioEvent
+{
+    DASH,
+    SHOT,
+    HURT,
+    DEATH,
+}
 
 [RequireComponent(typeof(AudioSource))]
 public class PlayerAudioManager : MonoBehaviour
 {
     private AudioSource audioSource;
-    [SerializeField] private AudioClip shootClip;
+    [SerializeField] private List<PlayerAudio> playerAudios;
+    //[SerializeField] private AudioClip shootClip;
     //[SerializeField] private AudioClip hurtClip;
     //[SerializeField] private AudioClip deathClip;
     //[SerializeField] private AudioClip meditationClip;
@@ -15,10 +25,30 @@ public class PlayerAudioManager : MonoBehaviour
     //public AudioClip HurtClip { get => hurtClip; set => hurtClip = value; }
     //public AudioClip DeathClip { get => deathClip; set => deathClip = value; }
     //public AudioClip MeditationClip { get => meditationClip; set => meditationClip = value; }
-    [SerializeField] private Hashtable audioHash;
+
+    private Dictionary<string, AudioClip> audioDictionary= new Dictionary<string, AudioClip>();
+    private void Start()
+    {
+        foreach (PlayerAudio playerAudio in playerAudios)
+        {
+            if(audioDictionary != null)
+                audioDictionary.Add(playerAudio.AudioName, playerAudio.AudioClip);
+        }
+    }
+
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio(string audioEvent)
+    {
+        if(audioDictionary[audioEvent] != null)
+        {
+            audioSource.clip = audioDictionary[audioEvent];
+            audioSource.Play();
+        }
+
     }
 
     public void PlayAudio(AudioClip audio, bool onLoop)
@@ -29,7 +59,7 @@ public class PlayerAudioManager : MonoBehaviour
     }
     public void PlayAudio(AudioClip audio)
     {
-        audioSource.clip = (AudioClip)audio;
+        audioSource.clip = audio;
         audioSource.Play();
     }
     public void StopAudio()
@@ -39,14 +69,28 @@ public class PlayerAudioManager : MonoBehaviour
 
 
 
+
+
 }
 
 
 [Serializable]
-public class Audio {
+public class PlayerAudio
+{
 
-    public Audio(string state, AudioClip clip)
+    [SerializeField] private string audioName;
+    [SerializeField] private AudioClip audioClip;
+
+    public string AudioName { get => audioName; set => audioName = value; }
+    public AudioClip AudioClip { get => audioClip; set => audioClip = value; }
+
+    public PlayerAudio(string eventName, AudioClip audioClip )
     {
+        this.audioName = eventName;
+        this.audioClip = audioClip;
 
     }
+
+
+
 }
